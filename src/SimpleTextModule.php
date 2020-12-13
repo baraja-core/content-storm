186040be21c3229a2d5896b8adc53932cbb80abb
Jan Barášek <janbarasek@gmail.com>
2020-12-13 23:31:35 +0100

    Fix PhpStan error

--- src/SimpleTextModule.php
+++ src/SimpleTextModule.php
@@ -10,7 +10,7 @@ final class SimpleTextModule implements Module
 	public function render(string $haystack, array $parameters = []): string
 	{
 		return '<div>'
-			. ($parameters ? 'Params: <code>' . htmlspecialchars(json_encode($parameters), ENT_QUOTES) . '</code><br>' : '')
+			. ($parameters ? 'Params: <code>' . htmlspecialchars((string) json_encode($parameters), ENT_QUOTES) . '</code><br>' : '')
 			. htmlspecialchars($haystack, ENT_QUOTES)
 			. '</div>';
 	}
